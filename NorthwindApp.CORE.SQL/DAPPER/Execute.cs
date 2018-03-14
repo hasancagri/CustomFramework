2cd9e0b53d0f67776c685c1179efc653b1bf0cb0
Hasan Çağrı Demiriz <hasancagridemiriz@gmail.com>
2018-03-14 13:40:59 +0300

    Add files via upload

---
+++ NorthwindApp.CORE.SQL/DAPPER/Execute.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace NorthwindApp.CORE.SQL.DAPPER
+{
+    public class Execute<TEntity>
+    {
+        SqlConnection _con;
+
+        public Execute()
+        {
+            //Burada şifreleme olacak
+            _con = new SqlConnection("Data source=ÇAĞRı-ÇAĞRı;initial catalog=NORTHWND;user Id=sa;password=hasan");
+        }
+
+        public List<TEntity> GetList(string query, object parameters)
+        {
+            return _con
+                .Query<TEntity>(query, parameters, commandType: CommandType.StoredProcedure).ToList();
+        }
+
+        public TEntity Get(string query, object parameters)
+        {
+            return _con
+                .QuerySingleOrDefault<TEntity>(query, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public T Scalar<T>(string query, object parameters)
+        {
+            return (T)Convert.ChangeType(_con.ExecuteScalar(query, parameters, commandType: CommandType.StoredProcedure), typeof(T));
+        }
+
+        public int ExecuteQuery(string query, object parameters)
+        {
+            return _con
+                .Execute(query, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
